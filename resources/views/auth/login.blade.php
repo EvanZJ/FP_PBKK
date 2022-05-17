8f363e13b85dfe3c3e63f52b4b8d81bcffcadb2f
unknown <januar.evan2012@gmail.com>
2022-05-17 20:09:12 +0700

    initial commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="/css/app.css" rel="stylesheet">
+    <title>Login</title>
+    <link href="http://fonts.cdnfonts.com/css/sf-pro-display" rel="stylesheet">
+</head>
+<body>
+    <section class="vh-100">
+        <div class="container-fluid">
+            <div class="row">
+            <div class="col-lg-6 text-black">
+        
+                <div class="px-5 ms-xl-4">
+                {{--  <i class="fas fa-crow fa-2x me-3 pt-5 mt-xl-4" style="color: #709085;"></i>  --}}
+                <img src="/logopbkk.png" class="img-thumbnail">
+                </div>
+        
+                <div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
+        
+                <form style="width: 39rem;" action="{{ route('login.custom') }}" method="POST">
+                    @csrf
+                    <h3 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">
+                        Log in
+                    </h3>
+        
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Email address</label>
+                        <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp">
+                        <div id="emailHelp" class="form-text">We will never share your email with anyone else.</div>
+                        @if ($errors->has('email'))
+                            <span class="text-danger">{{ $errors->first('email') }}</span>
+                        @endif
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="password" name="password">
+                        @if ($errors->has('password'))
+                            <span class="text-danger">{{ $errors->first('password') }}</span>
+                        @endif
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <br>
+                    <br>
+                    <p>Do not have an account? <a href="{{ route('register-user') }}" class="link-info">Register here</a></p>
+        
+                </form>
+        
+                </div>
+        
+            </div>
+            <div class="col-sm-6 px-0 d-none d-sm-block">
+                <img src="/page__en_us_15700878770.jpeg"
+                alt="Login image" class="w-10 vh-10" style="object-fit: cover; object-position: left;">
+            </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
